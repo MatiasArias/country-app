5ef601eca90849c5a69c4fc9c80e0234d64d4881
Matias Arias <matiasarias384@gmail.com>
2024-03-14 12:34:32 -0300

    Upgrade page

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,23 +1,45 @@
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
-import { ByCapitalPageComponent } from '../../../countries/pages/by-capital-page/by-capital-page.component';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { unsubscribe } from 'diagnostics_channel';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
-  styles: ``
+  styles: ``,
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+  private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSubscription?: Subscription;
 
+  ngOnInit(): void {
+    this.debouncerSubscription = this.debouncer
+      .pipe(debounceTime(300))
+      .subscribe((value) => {
+        this.onDebounce.emit(value);
+      });
+  }
 
+  ngOnDestroy(): void {
+    this.debouncerSubscription?.unsubscribe();
+  }
 
-@Input()
-public placeholder:string = '';
+  @Input()
+  public placeholder: string = '';
 
+  @Input()
+  public initialValue: string = '';
 
-searchTag(newSearch :string){
-this.onValue.emit(newSearch)
-}
+  searchTag(newSearch: string) {
+    this.onValue.emit(newSearch);
+  }
 
-@Output()
-public onValue = new EventEmitter<string>();
+  @Output()
+  public onValue = new EventEmitter<string>();
+
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+  onkeyPress(searchTerm: string) {
+    this.debouncer.next(searchTerm);
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,31 +3,29 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
-import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
-
-
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AboutPageComponent,
-    HomePageComponent,
-    SidebarComponent,
     ContactPageComponent,
+    HomePageComponent,
+    LoadingSpinnerComponent,
     SearchBoxComponent,
-    LoadingSpinnerComponent
+    SidebarComponent,
   ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
-  exports:[
-    HomePageComponent,
+  imports: [CommonModule, RouterModule],
+  exports: [
+    AboutPageComponent,
     ContactPageComponent,
-    AboutPageComponent,SearchBoxComponent,
-    SidebarComponent]
+    HomePageComponent,
+    LoadingSpinnerComponent,
+    SearchBoxComponent,
+    SidebarComponent,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
